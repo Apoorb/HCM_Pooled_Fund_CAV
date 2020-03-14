fa3dd12bf94feb2af955cafea37e9aeb422c872f
Apoorba Bibeka <abibeka@kittelson.com>
2020-03-14 16:22:32 -0400

    Allow Multiple Factors

--- AutomateExperimentalDesignArterialSimulationRun.py
+++ AutomateExperimentalDesignArterialSimulationRun.py
@@ -11,6 +11,7 @@ import win32com.client as com
 import os
 import glob
 import sys
+import shutil
 sys.path.append(r'C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc\Documents\Github\HCM_Pooled_Fund_CAV')
 
 Vissim = com.Dispatch("Vissim.Vissim-64.1100") # Vissim 9 - 64 bit
@@ -44,12 +45,36 @@ def SetMPRforVISSIM(MPR="0PerMPR"):
     for i in range(1,14):
         #print(Vissim.Net.VehicleInputs.ItemByKey(VI_number).AttValue("VehComp({})".format(i)))
         Vissim.Net.VehicleInputs.ItemByKey(VI_number).SetAttValue("VehComp({})".format(i),MPR_to_VissimVehCompMap[MPR])    
-    Filename = os.path.join(Path_to_VissimFile,"ArtBaseNet_{}.inpx".format(MPR))
+    return()
+    
+def SaveVissimFile(OuterDir, PltSz, Gap, Mpr):
+    Filename = os.path.join(OuterDir,"{}".format(PltSz),"{}".format(Gap)
+                            ,"ArtBaseNet_{}_{}_{}.inpx".format(PltSz,Gap,MPR))
     Vissim.SaveNetAs(Filename)
     Vissim.SaveLayout(Filename.replace('inpx','layx'))
     Nm = "ArtBaseNet_{}".format(MPR)
-    return(Nm)
+    return()
     
+def EditParamFile(OuterDir,Speed,PltSz,Gap):
+    ParamFile = os.path.join(OuterDir,"{}".format(PltSz),"{}".format(Gap),"parameters.txt")
+    file1 = open(ParamFile,"w") 
+    L1 = Speed * 0.44704; L2 = PltSz; L3 = Gap # Speed in m/s
+    file1.write("{}\n".format(L1)); file1.write("{}\n".format(L2)); file1.write("{}\n".format(L3))
+    file1.close()
+    return()
+    
+def CopyDriverModelDll(OuterDir,Speed,PltSz,Gap):
+    DriverModelDll = os.path.join(OuterDir,"DriverModel.dll")
+    NewLoc = os.path.join(OuterDir,"{}".format(PltSz),"{}".format(Gap))
+    shutil.copy(DriverModelDll, NewLoc)
+    return()
+
+def CopyRBCFile(OuterDir,Speed,PltSz,Gap):
+    RBCFile = os.path.join(OuterDir,"PreTime_Synchro_V1.rbc")
+    NewLoc = os.path.join(OuterDir,"{}".format(PltSz),"{}".format(Gap))
+    shutil.copy(RBCFile, NewLoc)
+    return()
+
 def RunVissimBatchModel():
     Vissim.Simulation.SetAttValue('NumRuns', 10)
     Vissim.Simulation.SetAttValue('UseMaxSimSpeed', True)
@@ -62,6 +87,9 @@ Vissim.Net.VehicleInputs.ItemByKey(VI_number).AttValue("VehComp(1)")
 
 Vissim.Net.VehicleInputs.ItemByKey(VI_number).AttValue("VehComp(2)")
 
+
+Gaps = [0.6, 1, 1.2]
+PlatoonSizes = [1,2,5,10]
 MPR_to_VissimVehCompMap ={
  "0PerMPR":1,
  "20PerMPR":2,
@@ -73,10 +101,32 @@ MPR_to_VissimVehCompMap ={
 VehComp = 1
 
 AlreadyRan = []
-for MPR in MPR_to_VissimVehCompMap.keys():
-    if MPR not in AlreadyRan:
-        Nm = SetMPRforVISSIM(MPR)
-        RunVissimBatchModel()
+# EditParamFile(OuterDir=Path_to_VissimFile,Speed= 40,PltSz=2,Gap=0.6)
+
+for PltSz_l in PlatoonSizes:
+    try:
+        os.mkdir(os.path.join(Path_to_VissimFile,"{}".format(PltSz_l)))
+    except:
+        print("Dir creation error")
+    for Gap_l in Gaps:
+        if (PltSz_l== 1) & (Gap_l in [1,1.2]):
+            continue
+        elif PltSz_l== 1:
+            Gap_l = 1.4
+        else: ""
+        try:
+            os.mkdir(os.path.join(Path_to_VissimFile,"{}".format(PltSz_l),"{}".format(Gap_l)))
+        except:
+            print("Dir creation error")  
+        EditParamFile(OuterDir=Path_to_VissimFile,Speed= 40,PltSz=PltSz_l,Gap=Gap_l)
+        CopyDriverModelDll(OuterDir=Path_to_VissimFile,Speed= 40,PltSz=PltSz_l,Gap=Gap_l)
+        CopyRBCFile(OuterDir=Path_to_VissimFile,Speed= 40,PltSz=PltSz_l,Gap=Gap_l)
+        for MPR in MPR_to_VissimVehCompMap.keys():
+            if MPR not in AlreadyRan:
+                SetMPRforVISSIM(MPR)
+                SaveVissimFile(OuterDir=Path_to_VissimFile, PltSz=PltSz_l, Gap=Gap_l, Mpr=MPR)
+                os.chdir(Path_to_VissimFile)
+                RunVissimBatchModel()
 ## ========================================================================
 # End Vissim
 #==========================================================================
